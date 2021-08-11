fc0fa9f6ee40802b2724936f532f46fd64a0d2c8
iamBin298 <88784356+iamBin298@users.noreply.github.com>
2021-08-11 21:45:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Cậu ơi <3'.,
+    introDesc: `Khoa muốn nói với cậu điều này'.,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Cậu làm bạn gái Khoa nha',
+    desc: 'Tớ thích cậu lâu rồi ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Tớ đồng ý nha <33',
+    btnNo: 'Không, Đéo nhé !!',
+    question:'Sao cậu chọn tớ vậy ? Cho tớ biết lí do nha',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tớ cho cậu cơ hội đó <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Hết dịch tớ dẫn cậu đi ăn nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com//
 }
